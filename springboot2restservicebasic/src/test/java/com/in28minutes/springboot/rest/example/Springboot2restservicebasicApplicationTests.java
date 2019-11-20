394a36e2aee71dbe654016df6d058e50ba8ac533
Kaleigh Jaeger <kjaeger123@gmail.com>
2019-11-20 10:40:03 -0600

    Rest Service Demo Initial Commit

---
+++ springboot2restservicebasic/src/test/java/com/in28minutes/springboot/rest/example/Springboot2restservicebasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.springboot.rest.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot2restservicebasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
